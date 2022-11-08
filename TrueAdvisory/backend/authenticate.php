3e93d850f33a8caed26c969f6a10f714ee2f433a
alexistucker <ajatucker@outlook.com>
2022-11-08 15:58:57 -0500

    updating
    fixed signin, deleting extraneous files

--- TrueAdvisory/backend/authenticate.php
+++ TrueAdvisory/backend/authenticate.php
@@ -12,29 +12,31 @@ if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
-if (!isset($_POST['username'], $_POST['password'])) {
+if (!isset($_POST['email'], $_POST['password'])) {
 	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password field!');
 }
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+if ($stmt = $con->prepare('SELECT id, password FROM user WHERE email = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST['username']);
+	$stmt->bind_param('s', $_POST['email']);
 	$stmt->execute();
 	// Store the result so we can check if the account exists in the database.
 	$stmt->store_result();
-	// If the username exiusts
+	// If the username exists
 	if ($stmt->num_rows > 0) {
 		$stmt->bind_result($id, $password);
 		$stmt->fetch();
 		// Account exists, now we verify the password.
 		// Note: remember to use password_hash in your registration file to store the hashed passwords.
-		if (password_verify($_POST['password'], $password)) {
+		//password_verify($_POST['password'], $password)
+		//$passwordHash = 
+		if ($_POST['password'] === $password) {
 			// Verification success! User has loggedin!
 			// Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
 			session_regenerate_id();
 			$_SESSION['loggedin'] = TRUE;
-			$_SESSION['name'] = $_POST['username'];
+			$_SESSION['name'] = $_POST['email'];
 			$_SESSION['id'] = $id;
 			header('Location: home.php');
 		} else {
@@ -43,6 +45,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 	} else {
 		echo 'Incorrect username!';
 	}
+	$stmt->close();
 } else {
 	echo 'Could not prepare statement!';
 }
