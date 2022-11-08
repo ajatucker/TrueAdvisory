2a9de461a34f75331f66601e2c2633dc57008fe5
alexistucker <ajatucker@outlook.com>
2022-11-08 12:55:17 -0500

    finished sign in?
    need to test this, and replace the profile.php with the new user home page we have and change the home redirection

--- HTMLPages/backend/authenticate.php
+++ HTMLPages/backend/authenticate.php
@@ -4,7 +4,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'trueadvisorydb';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
