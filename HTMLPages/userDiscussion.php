f39e63e8addd31b8445553562281fc0a3156ebd6
jdutzy <j.dutzy@outlook.com>
2022-11-08 17:27:44 -0500

    edited styling of the discussion chat box

--- HTMLPages/userDiscussion.php
+++ HTMLPages/userDiscussion.php
@@ -160,9 +160,6 @@ $statement3->closeCursor();
     
         </div>
     </div>
-    <div class="footer">
-        Powered by the University of Michigan - Dearborn and learning in CIS 435
-    </div>
 
     <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
